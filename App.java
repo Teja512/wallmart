31bc38f0020ded41ff13d975a48cc8db9922e566
Teja512 <62785409+Teja512@users.noreply.github.com>
2024-06-27 10:03:19 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+package com.gamutkart;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+		int j;
+
+		for(i=0;i<30;i++)
+		{
+			i += 5;
+			i += 5;
+   		}
+	 }
+}
